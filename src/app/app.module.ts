97b0efd6dc5962de282804f9f097c4d5c9305b1c
Javier Esteban <jaesmon@gmail.com>
2018-06-15 21:49:51 -0500

    Implementando bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { ModalModule } from 'ngx-bootstrap/modal';
 
 import { AppComponent } from './app.component';
 import { SomeComponentComponent } from './some-component/some-component.component';
@@ -8,7 +11,9 @@ import { MatCardModule } from '@angular/material/card';
 
 
 
+
 @NgModule({
+  
   declarations: [
     AppComponent,
     SomeComponentComponent,
@@ -17,9 +22,13 @@ import { MatCardModule } from '@angular/material/card';
   imports: [
     BrowserModule,
     MatCardModule,
+    BsDropdownModule.forRoot(),
+    TooltipModule.forRoot(),
+    ModalModule.forRoot()
     
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+export class AppBootstrapModule {}
