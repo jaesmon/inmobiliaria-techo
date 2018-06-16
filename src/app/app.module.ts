c9d4f40d8d86236713578a1defa70eac6f43b98d
LUADRUSI <luadrusi@yahoo.com>
2018-06-16 11:55:14 -0500

    Cambio presentacion incorporacion de librerias

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {  } from "module";
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -9,6 +11,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
